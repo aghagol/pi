2e13b6f86b29136f633a6f4577b6c1e61c9aff96
Mohammad Aghagolzadeh <mgolzade@gmail.com>
2016-05-31 18:38:23 -0700

    cooler interface

--- arrowkeys.py
+++ arrowkeys.py
@@ -1,14 +1,25 @@
 import curses
 
+s = ord('s')
+
+x = 1
+y = 1
+
 myscreen = curses.initscr()
 
-x = ord('s')
-
-while x!=ord('q'):
+while s!=ord('q'):
 	myscreen.clear()
 	myscreen.border(0)
-	myscreen.addstr(12, 25, chr(x))
+	myscreen.addstr(x, y, 'Move me! (or press \'q\' to quit)')
 	myscreen.refresh()
-	x = myscreen.getch()
+	s = myscreen.getch()
+	if chr(s)=='A' and x>0:
+		x -= 1
+	if chr(s)=='B' and x<22:
+		x += 1
+	if chr(s)=='C' and y<55:
+		y += 1
+	if chr(s)=='D' and y>0:
+		y -= 1
 
 curses.endwin()
