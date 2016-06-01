1809762b58ebed89658475eb977b61c338792b9c
Mohammad aghagolzadeh <mo@Mohammads-MacBook-Pro.local>
2016-05-31 20:08:42 -0700

    more complicated manual control

--- manual_navi.py
+++ manual_navi.py
@@ -7,23 +7,48 @@ s = ord('s')
 
 myscreen = curses.initscr()
 
+state = 'stop'
+speed_start = 50
+
 while s!=ord('q'):
+
 	myscreen.clear()
 	myscreen.border(0)
 	myscreen.refresh()
 	s = myscreen.getch()
+
 	if chr(s)=='A':
-		mover.forward(55)
-		# time.sleep(.5)
+		if state=='forward':
+			speed+=20
+		else:
+			speed=speed_start
+		state = 'forward'
+		mover.forward(speed)
+
 	if chr(s)=='B':
-		mover.backward(55)
-		# time.sleep(.5)
+		if state=='backward':
+			speed+=20
+		else:
+			speed=speed_start
+		state = 'backward'
+		mover.backward(speed)
+
 	if chr(s)=='C':
-		mover.rotate_right(55)
-		# time.sleep(.5)
+		if state=='rotate_right':
+			speed+=20
+		else:
+			speed=speed_start
+		state = 'rotate_right'
+		mover.rotate_right(speed)
+
 	if chr(s)=='D':
-		mover.rotate_left(55)
-		# time.sleep(.5)
+		if state=='rotate_left':
+			speed+=20
+		else:
+			speed=speed_start
+		state = 'rotate_left'
+		mover.rotate_left(speed)
+
 	if chr(s)==' ':
 		mover.stop()
 
