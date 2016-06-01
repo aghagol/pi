6194a58f263722e6cdc113868ca8ab9b132cf749
Mohammad Aghagolzadeh <mgolzade@gmail.com>
2016-05-31 18:23:59 -0700

    @#$%#$

--- arrowkeys.py
+++ arrowkeys.py
@@ -2,15 +2,13 @@ import curses
 
 myscreen = curses.initscr()
 
-x = ord('h')
+x = ord('s')
 
-while True:
+while x!=ord('q'):
 	myscreen.clear()
 	myscreen.border(0)
 	myscreen.addstr(12, 25, chr(x))
 	myscreen.refresh()
 	x = myscreen.getch()
-	if chr(x)=='q':
-		break
 
 curses.endwin()
