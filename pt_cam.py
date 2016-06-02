b195b1562e26c98d5789995239ab5c0fa83cd80e
Mohammad aghagolzadeh <mo@Mohammads-MacBook-Pro.local>
2016-06-02 00:53:43 -0700

    scan

--- pt_cam.py
+++ pt_cam.py
@@ -11,13 +11,21 @@ t=GPIO.PWM(18,50)
 
 p.start(5)
 t.start(5)
-for d in np.arange(5.5,10,.5):
-	p.ChangeDutyCycle(d)
-p.ChangeDutyCycle(5)
+sleep(.1)
 
 for d in np.arange(5.5,10,.5):
+	p.ChangeDutyCycle(d)
+	sleep(.1)
+p.ChangeDutyCycle(5)
+
+sleep(.1)
+for d in np.arange(5.5,10,.5):
 	t.ChangeDutyCycle(d)
+	sleep(.1)
 t.ChangeDutyCycle(5)
+
+p.stop()
+t.stop()
 
 GPIO.cleanup()
 # pwm.ChangeDutyCycle(7.5)
