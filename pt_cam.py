52d45d71f4c47a8afff2702a3bc86243a534f87f
Mohammad aghagolzadeh <mo@Mohammads-MacBook-Pro.local>
2016-06-02 00:50:46 -0700

    scan

--- pt_cam.py
+++ pt_cam.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import numpy as np
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16,GPIO.OUT)
 GPIO.setup(18,GPIO.OUT)
 
-pwm=GPIO.PWM(18,50)
-pwm.start(7.5)
-sleep(.1)
-pwm.stop()
+p=GPIO.PWM(16,50)
+t=GPIO.PWM(18,50)
+
+p.start(5)
+t.start(5)
+for d in np.arange(5.5,10,.5):
+	pwm.ChangeDutyCycle(d)
 
 GPIO.cleanup()
 # pwm.ChangeDutyCycle(7.5)
