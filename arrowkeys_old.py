9f859147be9e9cbf49117a0f8a7ca5c76beecac6
Mohammad Aghagolzadeh <mgolzade@gmail.com>
2016-07-18 08:32:22 -0700

    backup

--- arrowkeys_old.py
+++ arrowkeys_old.py
@@ -13,13 +13,9 @@ while s!=ord('q'):
 	myscreen.addstr(x, y, 'Move me! (or press \'q\' to quit)')
 	myscreen.refresh()
 	s = myscreen.getch()
-	if chr(s)=='A' and x>0:
-		x -= 1
-	if chr(s)=='B' and x<22:
-		x += 1
-	if chr(s)=='C' and y<55:
-		y += 1
-	if chr(s)=='D' and y>0:
-		y -= 1
+	if chr(s)=='A' and x>0	: x -= 1
+	if chr(s)=='B' and x<22	: x += 1
+	if chr(s)=='C' and y<55	: y += 1
+	if chr(s)=='D' and y>0	: y -= 1
 
 curses.endwin()
